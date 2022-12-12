f62c31ef4478bcbb03d3b9113b2ec25b21726112
eliza <eliza@DESKTOP-HLLIIPJ>
2022-12-12 23:24:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7_uspad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat(2, "Gray", 13);
+            Dog dog1 = new Dog(5, "Black", 10);
+
+            Console.WriteLine(cat1.ToString());
+            Console.WriteLine(cat1.MakeSound());
+            Console.WriteLine(dog1.ToString());
+            Console.WriteLine(dog1.MakeSound());
+
+            Console.ReadKey();
+        }
+    }
+}
